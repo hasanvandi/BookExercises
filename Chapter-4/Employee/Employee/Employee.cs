4b5647cee7046ae3b823d30294917f6197437502
hasanvandi <mahdihasanvandi2540@gmail.com>
2025-08-27 16:00:28 +0330

    chapter-4/employee added

---
+++ Chapter-4/Employee/Employee/Employee.cs
@@ -0,0 +1,39 @@
+﻿
+
+namespace Employee;
+
+    public class Employee
+    {
+    private string _name;
+    private string _lastName;
+
+    private decimal _monthlySalary;
+
+    const int Years = 12;
+    private decimal increasePercent = 10;
+    public Employee(string name,string lastName,decimal MonthlySalary)
+    {
+        _name = name;
+        _lastName = lastName;
+        _monthlySalary = MonthlySalary;
+    }
+
+
+    /// <summary>
+    /// محاسبه حقوق سالیانه
+    /// </summary>
+    public void ShowYearlySalary()
+    {
+        Console.WriteLine($"fullname:{_name} {_lastName}");
+        Console.WriteLine();
+        Console.WriteLine($"YearlySalary:{_monthlySalary * Years}");
+    }
+    /// <summary>
+    /// محاسبه حقوق سالیانه با افزایش ده درصدی
+    /// </summary>
+    public void ShowSalary()
+    {
+        Console.WriteLine($"YearlySalary with increase 10%:{_monthlySalary * (1 + increasePercent /100) * Years}");
+    }
+}
+
---
+++ Chapter-4/Employee/Employee/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Employee
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee("mahdi","hasanvandi",2500);
+            Employee employee1 = new Employee("alireza","oroumand",22000);
+
+
+            
+            employee.ShowYearlySalary();
+            employee.ShowSalary();
+            Console.WriteLine("-----------------------------------");
+            employee1.ShowYearlySalary();
+            employee1.ShowSalary();
+          
+            
+
+            Console.ReadLine();
+        }
+    }
+}
