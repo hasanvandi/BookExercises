398d35af0206895106325228b396406787be9bb4
hasanvandi <mahdihasanvandi2540@gmail.com>
2025-08-27 12:13:22 +0330

    add chapter-4 and invoice

---
+++ Chapter-4/Invoice/Invoice/Invoice.cs
@@ -0,0 +1,74 @@
+﻿
+
+namespace Invoice;
+
+public class Invoice
+{
+    public int PartNumber;
+    public string Descrioption;
+    public int NumberOfPieces;
+    public int Price;
+
+
+    public int GetPartNumber()
+    {
+        return PartNumber;
+    }
+    public void SetPartNumber(int value)
+    {
+        PartNumber = value;
+    }
+
+
+    public string GetDescrioption()
+    {
+        return Descrioption;
+    }
+    public void SetDescrioption(string value)
+    {
+        if(Descrioption != null)
+        {
+            Descrioption = value;
+        }
+        Descrioption = "please enter descrioption";
+    }
+
+
+
+    public int GetNumberOfPieces()
+    {
+      return NumberOfPieces;
+    }
+    public void SetNumberOfPieces(int value)
+    {
+        if (PartNumber < 0)
+        {
+            PartNumber = 0;
+        }
+        NumberOfPieces = value;
+    }
+
+
+
+    public int GetPrice()
+    {
+        return Price;
+    }
+    public void SetPrice(int value)
+    {
+       if(Price < 0)
+        {
+            Price = 0;
+        }
+       Price = value;
+    }
+
+
+
+
+    public int InvoiceAmount()
+    {
+        return NumberOfPieces * Price;
+    }
+}
+
---
+++ Chapter-4/Invoice/Invoice/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Invoice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Invoice invoice = new()
+            {
+                PartNumber = 1,
+                Descrioption ="laptop",
+                NumberOfPieces = 4,
+                Price = 600
+            };
+            Console.WriteLine(invoice.InvoiceAmount());
+
+            Console.ReadLine();
+        }
+    }
+}
