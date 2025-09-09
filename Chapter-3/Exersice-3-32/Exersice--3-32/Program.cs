494d7c95cdb71cf2ce7bb4b8158f9d86aaa383ec
hasanvandi <mahdihasanvandi2540@gmail.com>
2025-09-09 18:51:04 +0330

    Added Exercise-3-32 in chapter-3

---
+++ Chapter-3/Exersice-3-32/Exersice--3-32/Program.cs
@@ -0,0 +1,39 @@
+﻿int countPlas = 0;
+int countBlas= 0;
+int countZero = 0;
+for(int i = 0; i < 5; i++)
+{
+    Console.Write($"Plese Enter number{i + 1}:");
+    string input = Console.ReadLine();
+    int number;
+    bool successes = int.TryParse( input, out number );
+    if (successes)
+    {
+        if(number == 0)
+        {
+            countZero++;
+        }
+        else if(number > 0)
+        {
+            countPlas++;
+        }
+        else
+        {
+            countBlas++;
+        }
+    }
+    else
+    {
+        Console.WriteLine("please enter number correct");
+        i--;
+        continue;
+
+    }
+}
+
+
+Console.WriteLine("0 is:" + countZero);
+Console.WriteLine("+ is:" + countPlas);
+Console.WriteLine("- is:" + countBlas);
+
+Console.ReadLine();
